db295660bbf03241c369e45a03725d756533654c
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-18 20:19:01 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cont = 97;
+    char a;
+    while (cont <= 122){
+        a = cont;
+        cout << cont << " e " << a <<endl;
+        cont++;
+    }
+    return 0;
+}
